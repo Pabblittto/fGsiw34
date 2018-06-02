89478c176b5a97011c2431b73cd1bf39d826d432
Xiguana <jak.nic98@gmail.com>
2018-06-02 16:18:48 +0200

    kupa. dla rozwijanego menu kod zamieszczam w komencie glownego

--- Projekt/Projekt/MainWindow.xaml.cs
+++ Projekt/Projekt/MainWindow.xaml.cs
@@ -13,16 +13,51 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Projekt
+namespace WpfApp1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+
             InitializeComponent();
+           
+
+
+        }
+
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
+    /* to do wsadzenia w wpf moje poprzednie menu wyswietla nie wiem jak je polaczyc
+     * <Window x:Class="Projekt.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:Projekt"
+        mc:Ignorable="d"
+        Title="MainWindow" Height="450" Width="800">
+    <Menu Margin="10,0,726,377">
+        <MenuItem Header="Dodaj..." Height="32" Width="52" Click="MenuItem_Click">
+            <MenuItem Header="Lotnisko"/>
+            <MenuItem Header="Trasa"/>
+            <MenuItem Header="Lot"/>
+            <MenuItem Header="Klient"/>
+            <MenuItem Header="Samolot">
+                <MenuItem Header="Typ"/>
+                <MenuItem Header="Model"/>
+            </MenuItem>
+            <Separator/>
+            <MenuItem Header="Rezerwacja"/>
+        </MenuItem>
+    </Menu>
+</Window>
+*/
 }
